daba0ac10d4067fff79c7c38717a8777ccc206f9
Antony Roshan <yozziedummy@gmail.com>
2025-04-02 12:36:08 +0530

    Add font and hero images to dashboard component

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from 'next/font/google'; // Primary font for the purposes of this application
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
